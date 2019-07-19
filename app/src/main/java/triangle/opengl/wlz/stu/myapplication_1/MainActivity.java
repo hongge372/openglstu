146babac616662fba266e918dbb4cc0ee06c12e7
hongge372 <hongge372@126.com>
2019-07-19 17:49:33 +0800

    mov costomview to its own floder

--- app/src/main/java/triangle/opengl/wlz/stu/myapplication_1/MainActivity.java
+++ app/src/main/java/triangle/opengl/wlz/stu/myapplication_1/MainActivity.java
@@ -1,18 +1,44 @@
 package triangle.opengl.wlz.stu.myapplication_1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-import triangle.opengl.wlz.stu.myapplication_1.gl.DGLView;
+import triangle.opengl.wlz.stu.myapplication_1.gl.CostomView;
 
 public class MainActivity extends AppCompatActivity {
-    triangle.opengl.wlz.stu.myapplication_1.gl.DGLView view;
     private final String TAG = getClass().getName();
+
+    private int size = 1;
+    Button[] button = new Button[size];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.layout_main);
+
+        layout_set();
+    }
+
+    private void layout_set(){
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                switch (v.getId()){
+                    case R.id.costom_glview:
+                        intent.setClass(getApplicationContext(), CostomView.class);
+                        break;
+                }
+                startActivity(intent);
+            }
+        };
+
+        button[0] = (Button)findViewById(R.id.costom_glview);
+        for(int i=0;i<size;i++){
+            button[i].setOnClickListener(listener);
+        }
     }
 }
---
+++ app/src/main/java/triangle/opengl/wlz/stu/myapplication_1/gl/CostomView.java
@@ -0,0 +1,14 @@
+package triangle.opengl.wlz.stu.myapplication_1.gl;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import triangle.opengl.wlz.stu.myapplication_1.R;
+
+public class CostomView extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.costom_glview);
+    }
+}
